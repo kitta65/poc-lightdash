3b2726e39c0162e6e6a3702be03f8d1887128af0
Dairyo Sekine <d.sekine@cluster.mu>
2023-12-31 01:05:09 +0900

    install lightdash cli

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,13 @@
 set -euo pipefail
 cd $(dirname $0)/.. # project root
 
+# dbt
 rm -rf venv
 python -m venv venv
 source ./venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 deactivate
+
+# lightdash
+npm install -g @lightdash/cli
