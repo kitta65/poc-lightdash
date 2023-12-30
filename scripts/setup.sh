191ffd3de80bcab5a5621023efa08dd1cece662c
Dairyo Sekine <d.sekine@cluster.mu>
2023-12-30 10:20:39 +0900

    install dbt

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+cd $(dirname $0)/.. # project root
+
+rm -rf venv
+python -m venv venv
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
